3f2d410dc3578b3df854a6461ceb79f46386c65e
ifemalomo <malomoife@gmail.com>
2024-06-17 23:49:44 -0400

    added duplicate check, dates, and delete button to to-do list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,12 +19,24 @@ class Home extends Component {
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
+    if (this.state.todos.find((t) => t.content === todo.content)){
+      return
+    }
+    
     todo.id = Math.random();
     // Create a array that contains the current array and the new todo item
     let new_list = [...this.state.todos, todo];
     // Update the local state with the new array.
     this.setState({
       todos: new_list,
+    });
+  };
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: todos,
     });
   };
   render() {
@@ -34,6 +46,7 @@ class Home extends Component {
         {/* When passing the AddTodo component, addTodo is a prop that is used in the 
         AddTodo.js file when handling the submit */}
         <AddTodo addTodo={this.addTodo} />
+        <Home deleteTodo={this.deleteTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
         <Todos todos={this.state.todos} />
